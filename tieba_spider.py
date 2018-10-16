b93b63470955ee47c60c6b18fad85509e5bf6ccc
BruceChenY <1522045070@qq.com>
2018-10-16 15:17:40 +0800

    Update tieba_spider.py

--- tieba_spider.py
+++ tieba_spider.py
@@ -3,9 +3,9 @@ import requests
 import re
 import os
 import time
-time.sleep(time_sleep)
+
 tieba_name=''
-time_sleep=0.5
+time_sleep=1
 
 class Scheduler():
     '''
@@ -17,11 +17,11 @@ class Scheduler():
         self.queue_already=[]
 
     def add_url(self,url):
-        if url not in self.queue_readly and url not in self.queue_already:
+        if (url not in self.queue_readly) and (url not in self.queue_already):
             self.queue_readly.append(url)
 
     def get_url(self):
-        if len(self.queue_already)>0:
+        if len(self.queue_readly)>0:
             url=self.queue_readly.pop(0)
             self.queue_already.append(url)
             return url
@@ -40,6 +40,8 @@ class Spider():
     def __init__(self):
         self.scheduler_page=Scheduler()
         self.scheduler_list=Scheduler()
+        self.img_name=0 
+        self.list_count=0
     def get_page_url(self):
         url='https://tieba.baidu.com/f?kw='+tieba_name
         res=requests.get(url)
@@ -51,28 +53,32 @@ class Spider():
         
         base_url='https://tieba.baidu.com/f?kw='+tieba_name+'&ie=utf-8&pn='
         for i in ran_page:
-            slef.scheduler_page.add_url(base_url+str(i))
+            self.scheduler_page.add_url(base_url+str(i))
         print(tieba_name+'共'+str(len(ran_page))+'页','url已经获取完成')
+        print('已获取帖子链接%5d'%0,end='')
             
     def get_list_url(self):
         while True:
             url=self.scheduler_page.get_url()
-            if res is None:
+            if url is None or self.list_count>5:
                 break
+            self.list_count+=1
             time.sleep(time_sleep)
             res=requests.get(url)
             text=res.text
             li=re.findall(r'href=\"(/p/\d+)',text)
             base_url='https://tieba.baidu.com'
             for i in li:
-               self.scheduler_list.addurl(base_url+i) 
+               self.scheduler_list.add_url(base_url+i)
+               print('\b'*5,end='')
+               print('%5d'%self.scheduler_list.len_readly()) 
         print(tieba_name+'共'+str(self.scheduler_list.len_readly())+'贴','url已经获取完成')
-        print('已下载图片%5d'%0,'剩余贴页面数量%5d'%0,end='')
+        print('已下载图片%5d'%0,'剩余贴页面数量%5d'%self.scheduler_list.len_readly(),end='')
            
     def get_img_url(self):
         while True:
             url=self.scheduler_list.get_url()
-            if res is None:
+            if url is None:
                 return
             time.sleep(time_sleep)
             res=requests.get(url)
@@ -82,20 +88,19 @@ class Spider():
                 page_count=int(m.group(1))
                 if page_count>1:
                     for i in range(2,page_count+1):
-                        self.scheduler_list.addurl(url+'?pn='+str(i))
+                        self.scheduler_list.add_url(url+'?pn='+str(i))
             
             li_img_url=re.findall(r'<img class="BDE_Image" pic_type="0".*?src="(.*?jpg)"',text)
             for i in li_img_url:
                 time.sleep(time_sleep)
                 res=requests.get(i)
-                f=open('./img/'+str(img_name)+'.jpg','wb')
-                img_name+=1
+                f=open('./img/'+str(self.img_name)+'.jpg','wb')
+                self.img_name+=1
                 f.write(res.content)
                 f.close()
                 print('\b'*23)
-                print('已下载图片%5d'%img_name,'剩余贴页面数量%5d'%self.self.scheduler_list.len_readly(),end=''
-                
-                
+                print('已下载图片%5d'%self.img_name,'剩余贴页面数量%5d'%self.scheduler_list.len_readly(),end='')
+           
     def start_spider(self):
         self.get_page_url()
         self.get_list_url()
